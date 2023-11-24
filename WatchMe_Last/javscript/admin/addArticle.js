db2063c73601154ffd0f1364587f770de6551b1e
SaraUlan <220872@astanait.edu.kz>
2023-11-24 09:57:46 +0600

    new article img

--- WatchMe_Last/javscript/admin/addArticle.js
+++ WatchMe_Last/javscript/admin/addArticle.js
@@ -7,31 +7,40 @@ document
     var continuance = document.getElementById("continuance").value;
     var directedBy = document.getElementById("directedBy").value;
     var genre = document.getElementById("genre").value;
-    var image = document.getElementById("image").value;
-    var imageFile = imageInput.files[0];
+    var imageInput = document.getElementById("image");
     var about = document.getElementById("about").value;
-
     var filmData = {
       name: filmName,
       continuance: continuance,
       directedBy: directedBy,
+      image: "",
       genre: genre,
-      image: image,
       about: about,
     };
-    
-    if (typeof Storage !== "undefined") {
-      var films = JSON.parse(localStorage.getItem("films")) || [];
-      films.push(filmData);
-      localStorage.setItem("films", JSON.stringify(films));
+    if (imageInput.files && imageInput.files[0]) {
+      var file = imageInput.files[0];
 
-      var catalogImage = document.getElementById("image");
-      catalogImage.src = image;
+      var reader = new FileReader();
 
-      window.location.href = "catalog.html";
-    } else {
-      alert(
-        "Sorry, your browser does not support Web Storage. Please use a modern browser."
-      );
+      reader.onload = function (e) {
+        var imageData = e.target.result;
+        filmData.image = imageData;
+        var films = JSON.parse(localStorage.getItem("films")) || [];
+        films.push(filmData);
+        localStorage.setItem("films", JSON.stringify(films));
+
+        var catalogImage = document.getElementById("image");
+        catalogImage.src = filmData.image; 
+
+        console.log(filmData.image);
+      };
+      if (typeof Storage !== "undefined") {
+        window.location.href = "catalog.html";
+      } else {
+        alert(
+          "Sorry, your browser does not support Web Storage. Please use a modern browser."
+        );
+      }
+      reader.readAsDataURL(file);
     }
   });
--- WatchMe_Last/javscript/catalog/display.js
+++ WatchMe_Last/javscript/catalog/display.js
@@ -2,10 +2,13 @@ var films = JSON.parse(localStorage.getItem("films")) || [];
 const filmList = document.getElementById("page__film-left");
 
 films.forEach(function (film) {
-  var item = `
-        <div class="page__films">
-            <img src="images/${film.image}" alt="" class="film__img">
-            <ul class="list__film">
+  var item =
+    `
+        <div class="page__films" id="${film.image}">
+             <img src="` +
+    film.image +
+    `" alt="" id="$()" class="film__img">
+                <ul class="list__film">
                 <li class="film__title film__info">
                     <a href="detailPage.html" class="link-light">${film.name}</a>
                 </li>
@@ -17,6 +20,6 @@ films.forEach(function (film) {
                 </li>
             </ul>
         </div>`;
-
   filmList.innerHTML += item;
+  console.log(localStorage.getItem("films"));
 });
